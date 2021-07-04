f3596815df001fbe67c2ca6b9fc4a3f021967c7c
Leandro Palmieri <p@beetpix.com>
2021-07-05 10:10:48 +1200

    updating config

--- _config.php
+++ _config.php
@@ -2,12 +2,14 @@
 
 use SilverStripe\Assets\File;
 use SilverStripe\Assets\Image;
-use Intervention\Image\ImageManager;
 use SilverStripe\Core\Config\Config;
 
-$extensions = Config::inst()->get(File::class, 'class_for_file_extension');
+try {
 
-Config::inst()->set(File::class, 'class_for_file_extension', array_merge($extensions ?: [], [
-    'webp' => Image::class
-]));
+    $extensions = Config::inst()->get(File::class, 'class_for_file_extension');
 
+    Config::inst()->set(File::class, 'class_for_file_extension', array_merge($extensions ?: [], [
+        'webp' => Image::class
+    ]));
+
+} catch (Throwable $e) {}
