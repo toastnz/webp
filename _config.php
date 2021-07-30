a6ac22a3e44a80781ace4f73ff3f975ce49d0b9b
palmieri <p@beetpix.com>
2021-07-30 12:59:29 +1200

    modifying injector conditions

--- _config.php
+++ _config.php
@@ -2,7 +2,19 @@
 
 use SilverStripe\Assets\File;
 use SilverStripe\Assets\Image;
+use SilverStripe\Control\Director;
 use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Injector\Injector;
+
+
+$inject = !((!isset($_SERVER['REQUEST_URI']) || (isset($_SERVER['REQUEST_URI']) && ($_SERVER['REQUEST_URI'] == '/dev/build'))) || Director::is_cli());
+
+// Manipulation only matters when running it from the browser, and not a dev/build
+if ($inject) {
+    Config::inst()->update(Injector::class, Image::class, [
+        'class' => \Intervention\Image\WebP\Injectors\Image::class
+    ]);
+}
 
 try {
 
